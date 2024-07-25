ac4040875a1cfcefc43c03b14a685aa742989d87
hw22903 <hw22903@bristol.ac.uk>
2024-07-25 16:32:52 +0100

    Removed package line that was not being used

--- src/test/java/CommandTests.java
+++ src/test/java/CommandTests.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.alexmerz.graphviz.ParseException;
-import com.sun.tools.javac.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;

caf053ef02e440c9348e030de53302405cd69aca
Joe Dobson <lg22268@bristol.ac.uk>
2024-07-25 17:12:17 +0200

    Reapply "Merge branch 'dev' into write-action-matcher"
    This reverts commit 3e57ebaab1bb9ae9395513bf535e077858a5d6b7.

--- src/test/java/CommandTests.java
+++ src/test/java/CommandTests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.alexmerz.graphviz.ParseException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +16,11 @@ final class CommandTests {
   @BeforeEach
   void setup() {
       server = new GameServer();
+  }
+
+  @AfterEach
+  void reset() throws ParseException {
+    server.handleCommand("Daniel: reset");
   }
 
   @Test
@@ -32,6 +38,54 @@ final class CommandTests {
     String response = server.handleCommand("Daniel: inv");
     assertTrue((response.contains("You have the following items in your inventory") ||
             response.contains("You have no items in your inventory")), "Inventory not listed");
+  }
+
+  @Test
+  void testGetCommand() throws ParseException {
+    String response = server.handleCommand("Daniel: get potion");
+    assertTrue(response.contains("You now have 'potion' in your inventory"));
+    response = server.handleCommand("Daniel: get asdf");
+    assertTrue(response.contains("is not available to take"));
+    response = server.handleCommand("Daniel: get");
+    assertTrue(response.contains("You must provide a valid artefact you wish to get"));
+  }
+
+  @Test
+  void testDropCommand() throws ParseException {
+    String response = server.handleCommand("Daniel: drop potion");
+    assertTrue(response.contains("is not in your inventory"));
+    server.handleCommand("Daniel: get potion");
+    response = server.handleCommand("Daniel: drop potion");
+    System.out.println("LOWE " + response);
+    assertTrue(response.contains("You have dropped"));
+    response = server.handleCommand("Daniel: drop");
+    assertTrue(response.contains("You must provide a valid artefact you wish to drop from your inventory"));
+  }
+
+  @Test
+  void testGotoCommand() throws ParseException {
+    String response = server.handleCommand("Daniel: goto");
+    assertTrue(response.contains("You must provide a valid location you wish to move to"));
+    response = server.handleCommand("Daniel: goto asdf");
+    assertTrue(response.contains("is not a location you can travel to"));
+    response = server.handleCommand("Daniel: goto forest");
+    assertTrue(response.contains("You have moved to"));
+  }
+
+  @Test
+  void testResetCommand() throws ParseException {
+    String response = server.handleCommand("Daniel: reset asdf");
+    assertTrue(response.contains("To reset the game please simply type 'reset'"));
+    response = server.handleCommand("Daniel: goto forest");
+    assertTrue(response.contains("You have moved to"));
+    response = server.handleCommand("Daniel: get key");
+    assertTrue(response.contains("You now have 'key' in your inventory"));
+    response = server.handleCommand("Daniel: reset");
+    assertTrue(response.contains("Game reset"));
+    response = server.handleCommand("Daniel: look");
+    assertTrue(response.contains("cabin"));
+    response = server.handleCommand("Daniel: inv");
+    assertTrue(response.contains("You have no items in your inventory"));
   }
 
   @Test
